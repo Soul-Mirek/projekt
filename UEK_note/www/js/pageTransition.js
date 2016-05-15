a36c39d0c14e14fc50331da621e27970358d0276
Michał <michal.l.mirowski@gmail.com>
2016-05-15 23:38:18 +0200

    koniec

---
+++ UEK_note/www/js/pageTransition.js
@@ -0,0 +1,16 @@
+function addNote()
+{
+    window.location = "index.html";
+}
+function printNotePage()
+{
+    window.location = "index.html";
+}
+function modifyNotePage()
+{
+	window.location = "index.html"
+}
+function mainPage()
+{
+	window.location = "index.html";
+}
